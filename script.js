e9d394d3329fbd0c066b735ddf4179f1b1be6455
Prashant Singh <prashant.singh@aquera.com>
2025-07-11 12:33:54 +0530

    Cutomize

--- script.js
+++ script.js
@@ -55,15 +55,14 @@
 */
 const messages = [
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
+    "Really sure babes??",
+    "Ms Watermelon thinks again baba...",
+    "Just think about yours truly pucchi!",
+    "If dont miss me, I will be really sad...",
+    "I will be very sad... like a lot sad",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ok fine, I will stop..",
+    "Haa!! never gonna stop, Miss me my cute mepali lady! ❤️"
 ];
 
 let messageIndex = 0;
